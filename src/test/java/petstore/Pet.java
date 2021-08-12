96564d8e7f72fc48eea4196f38f52a9e88718718
Tatiane Almeida <almeidas.tatiane@gmail.com>
2021-08-12 20:27:22 -0300

    Incluso assertivas para o body do teste incluirPet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 public class Pet {
     String uri = "https://petstore.swagger.io/v2/pet";
@@ -28,6 +29,8 @@ public class Pet {
        .then()
                .log().all()
                .statusCode(200)
+               .body("name",is("Totó"))
+               .body("status", is("available"))
        ;
     }
 
